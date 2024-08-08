001dd5b190b8456c2a37bb043faa99ca1f4f6c33
velinamons <velinamons@gmail.com>
2024-08-08 17:49:06 +0300

    add Course, Lesson, Quiz, Question and Answer models

--- courses/models.py
+++ courses/models.py
@@ -1,2 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, related_name='lessons', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Quiz(models.Model):
+    lesson = models.ForeignKey(Lesson, related_name='quizzes', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
