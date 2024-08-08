4bc562ae2df9b6b121748053091c506973c54895
velinamons <velinamons@gmail.com>
2024-08-08 18:04:03 +0300

    register models in admin and apply migrations

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course, Lesson, Quiz, Question, Answer
 
-# Register your models here.
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
